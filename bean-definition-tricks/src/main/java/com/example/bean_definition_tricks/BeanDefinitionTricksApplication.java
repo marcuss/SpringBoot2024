fc501d609ecb6bb14fd2dad1258bc5871602d237
Marcus Sanchez <marcus.sanchez@viking.com>
2024-09-18 21:10:59 -0500

    Initial Commit

---
+++ bean-definition-tricks/src/main/java/com/example/bean_definition_tricks/BeanDefinitionTricksApplication.java
@@ -0,0 +1,13 @@
+package com.example.bean_definition_tricks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanDefinitionTricksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanDefinitionTricksApplication.class, args);
+	}
+
+}
---
+++ bean-definition-tricks/src/test/java/com/example/bean_definition_tricks/BeanDefinitionTricksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bean_definition_tricks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanDefinitionTricksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
